01d865445d05c0611a0b701043d0ff75afe2890b
thiagoferrer <thi-ferrer@live.com>
2025-08-14 16:19:16 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Clock brlClock = new BRLClock();
+        brlClock.setSecond(0);
+        brlClock.setMinute(0);
+        brlClock.setHour(15);
 
+        System.out.println(brlClock.getTime());
+
+        System.out.println(new USClock().convert(brlClock).getTime());
     }
 }
